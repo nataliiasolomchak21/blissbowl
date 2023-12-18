7ef0007746b872bf1dde3fb6815b6017a20754c9
nataliiasolomchak21 <natalia2001727@gmail.com>
2023-12-18 19:19:57 +0000

    Add Procfile

--- blissbowl/settings.py
+++ blissbowl/settings.py
@@ -10,11 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from pathlib import Path
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blissbowl.heroku.com', '127.0.0.1']
 
 
 # Application definition
