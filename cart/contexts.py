be2a7cc0c457a4ef0f132141be99d6bfa493fab7
nataliiasolomchak21 <natalia2001727@gmail.com>
2023-12-30 18:46:00 +0000

    Fix contexts file, add script for quantity

--- cart/contexts.py
+++ cart/contexts.py
@@ -10,14 +10,15 @@ def cart_contents(request):
     product_count = 0
     cart = request.session.get('cart', {})
 
-    for item_id, quantity in cart.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        cart_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product' : product
+    for item_id, item_data in cart.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            cart_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product' : product
         })
 
 
