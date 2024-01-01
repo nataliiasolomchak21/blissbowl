af2e7487889236e39242f465a723583615351e3f
nataliiasolomchak21 <natalia2001727@gmail.com>
2024-01-01 21:23:52 +0000

    Add form submit functionality

--- checkout/views.py
+++ checkout/views.py
@@ -7,6 +7,9 @@ from cart.contexts import cart_contents
 import stripe
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There's nothing in your cart at the moment")
@@ -15,13 +18,24 @@ def checkout(request):
     current_cart = cart_contents(request)
     total = current_cart['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+    
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51OJyBZLDXfeZuIluVChtkPuGC18AfmecBXtdey7YLn34t73syTUDqMb3cvgbn2KGEd9yVgkh77Zk41GLOzg5a1yY007YAya5ge',
-        'client_secret': 'test client secret'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
