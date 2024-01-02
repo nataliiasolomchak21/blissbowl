8f09349dc9f99547cbba1d685ad444572ff0b54e
nataliiasolomchak21 <natalia2001727@gmail.com>
2024-01-02 13:43:36 +0000

    Fix database parse  error

--- blissbowl/settings.py
+++ blissbowl/settings.py
@@ -128,7 +128,7 @@ WSGI_APPLICATION = "blissbowl.wsgi.application"
 # }
 
 DATABASES = {
-    "default": dj_database_url.parse(
+    "default": dj_database_url(
         os.environ.get("DATABASE_URL"), conn_max_age=600, options={"timezone": "UTC"}
     ),
 }
