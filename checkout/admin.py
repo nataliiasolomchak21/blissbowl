57d3575159a6d9ee3aacae9630d0168232453b70
nataliiasolomchak21 <natalia2001727@gmail.com>
2024-01-02 22:12:29 +0000

    Update webhook handler

--- checkout/admin.py
+++ checkout/admin.py
@@ -14,6 +14,8 @@ class OrderAdmin(admin.ModelAdmin):
         "delivery_cost",
         "order_total",
         "grand_total",
+        "original_cart",
+        "stripe_id",
     )
 
     fields = (
@@ -30,6 +32,8 @@ class OrderAdmin(admin.ModelAdmin):
         "delivery_cost",
         "order_total",
         "grand_total",
+        "original_cart",
+        "stripe_id",
     )
 
     list_display = (
