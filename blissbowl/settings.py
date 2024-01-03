6182910660759e056a8108b8e2b5a9c861553288
nataliiasolomchak21 <natalia2001727@gmail.com>
2024-01-03 09:31:34 +0000

    Import stripe to settings.py

--- blissbowl/settings.py
+++ blissbowl/settings.py
@@ -16,6 +16,8 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
+import stripe
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
