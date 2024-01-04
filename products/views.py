eaf4b37604c08c6280b3617a42cd8fb64e5ac410
nataliiasolomchak21 <natalia2001727@gmail.com>
2024-01-04 22:26:35 +0000

    Fix image field

--- products/views.py
+++ products/views.py
@@ -60,7 +60,7 @@ def add_product(request):
     """ Add a product to the store """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse('home'))
+        return redirect(reverse('homepage'))
     
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
@@ -85,7 +85,7 @@ def edit_product(request, product_id):
     """ Edit a product in the store """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse('home'))
+        return redirect(reverse('homepage'))
     
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
@@ -112,7 +112,7 @@ def edit_product(request, product_id):
 def delete_product(request, product_id):
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
-        return redirect(reverse('home'))
+        return redirect(reverse('homepage'))
     
     product = product = get_object_or_404(Product, pk=product_id)
     product.delete()
