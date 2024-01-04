96ed4d046468d8a07bcfca4001e8681782829497
nataliiasolomchak21 <natalia2001727@gmail.com>
2024-01-04 13:21:02 +0000

    Add product form

---
+++ products/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Product, Category
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+
+        # Use a list of tuples directly from the Category model
+        self.fields['category'].choices = [(c.id, str(c)) for c in categories]
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
