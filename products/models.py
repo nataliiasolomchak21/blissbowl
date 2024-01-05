8b83190150f83f287536d5272380b65635e1c056
nataliiasolomchak21 <natalia2001727@gmail.com>
2024-01-05 20:56:35 +0000

    Create Comment model and add form to produc detail template

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -27,3 +28,13 @@ class Product(models.Model):
 
     class Meta:
         verbose_name_plural = "Products"
+
+
+class Comment(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=90)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.product.name} - {self.created_at}'
