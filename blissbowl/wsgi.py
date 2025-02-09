411bf4f82a6f9e5b93c378cfd56ba097e986ba08
nataliiasolomchak21 <natalia2001727@gmail.com>
2025-02-09 19:21:18 +0000

    Update wsgi.py

--- blissbowl/wsgi.py
+++ blissbowl/wsgi.py
@@ -2,19 +2,17 @@
 WSGI config for blissbowl project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blissbowl.settings')
 
+# WSGI application callable
 application = get_wsgi_application()
 
-# vercel config
-
-app = application 
+# Expose the WSGI callable as `app` for Vercel
+app = application
