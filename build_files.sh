af32f2cee31dfa028b7114c54cc6740f3664c343
nataliiasolomchak21 <natalia2001727@gmail.com>
2025-02-09 19:25:32 +0000

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
-pip install -r requirements.txt
-python3.x manage.py collectstatic --no-input --clear
+#!/bin/bash
+
+# Ensure pip is installed and upgraded
+python3 -m pip install --upgrade pip
+
+# Install dependencies from requirements.txt
+python3 -m pip install -r requirements.txt
+
+# Collect static files using the default python version
+python3 manage.py collectstatic --no-input --clear
