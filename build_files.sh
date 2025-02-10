f38f5ac16c39516280c2b4107a1de051e1caae54
nataliiasolomchak21 <natalia2001727@gmail.com>
2025-02-10 21:21:27 +0000

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,30 +1,18 @@
 #!/bin/bash
 
-# Ensure pip is installed and upgraded
+# Ensure the script has execution permissions
+chmod +x build_files.sh
+
+# Create and activate a virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Ensure pip is installed and updated inside the virtual environment
 python3 -m ensurepip --default-pip
 python3 -m pip install --upgrade pip
 
-# Check Python version
-echo "Python version:"
-python3 --version
+# Install dependencies from requirements.txt inside the virtual environment
+pip install -r requirements.txt
 
-# Check pip version
-echo "Pip version:"
-python3 -m pip --version
-
-# Install dependencies
-python3 -m pip install --no-cache-dir -r requirements.txt
-
-python3 -m pip install Django==5.1
-python3 -m pip install dj-database-url==0.5.0
-python3 -m pip install psycopg2-binary==2.8.5
-python3 -m pip install dj3-cloudinary-storage==0.0.6
-python3 -m pip install django-allauth==65.3.1
-
-
-# Debugging: Check if Django is installed
-echo "Installed packages:"
-python3 -m pip list
-
-# Collect static files
+# Collect static files for Django
 python3 manage.py collectstatic --noinput
