7a58b82bbaf6249971002c1cfe5794c818b193ea
nataliiasolomchak21 <natalia2001727@gmail.com>
2025-02-10 21:18:21 +0000

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# Ensure the script has execution permissions
-chmod +x build_files.sh
-
-# Ensure pip is installed and updated
+# Ensure pip is installed and upgraded
 python3 -m ensurepip --default-pip
 python3 -m pip install --upgrade pip
-
-# Install dependencies from requirements.txt
-pip install -r requirements.txt
 
 # Check Python version
 echo "Python version:"
@@ -18,11 +12,19 @@ python3 --version
 echo "Pip version:"
 python3 -m pip --version
 
+# Install dependencies
+python3 -m pip install --no-cache-dir -r requirements.txt
+
 python3 -m pip install Django==5.1
 python3 -m pip install dj-database-url==0.5.0
 python3 -m pip install psycopg2-binary==2.8.5
 python3 -m pip install dj3-cloudinary-storage==0.0.6
+python3 -m pip install django-allauth==65.3.1
 
+
+# Debugging: Check if Django is installed
+echo "Installed packages:"
+python3 -m pip list
 
 # Collect static files
 python3 manage.py collectstatic --noinput
