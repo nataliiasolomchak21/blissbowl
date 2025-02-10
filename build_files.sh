1367d6b41025f9c08676a81837a690c064b325d7
nataliiasolomchak21 <natalia2001727@gmail.com>
2025-02-10 21:55:07 +0000

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -16,9 +16,11 @@ pip install -r requirements.txt
 
 python3 -m pip install Django==5.1
 python3 -m pip install dj-database-url==0.5.0
-python3 -m pip psycopg2-binary==2.8.5
-python3 -m pip cloudinary==1.36.0
-python3 -m pip django-allauth==65.3.1
+python3 -m pip install psycopg2-binary==2.8.5
+python3 -m pip install cloudinary==1.36.0
+python3 -m pip install django-allauth==65.3.1
+python3 -m pip install dj3-cloudinary-storage==0.0.6
+
 
 
 # Collect static files for Django
