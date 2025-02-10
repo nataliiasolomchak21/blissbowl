e86a86de446e7170bf4e3d68d5ccc6a1b74c64ae
nataliiasolomchak21 <natalia2001727@gmail.com>
2025-02-10 21:23:57 +0000

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -14,5 +14,8 @@ python3 -m pip install --upgrade pip
 # Install dependencies from requirements.txt inside the virtual environment
 pip install -r requirements.txt
 
+python3 -m pip install Django==5.1
+
+
 # Collect static files for Django
 python3 manage.py collectstatic --noinput
