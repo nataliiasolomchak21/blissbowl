5916ec8bf96ecd2df986021d8fd659b59dc34785
nataliiasolomchak21 <natalia2001727@gmail.com>
2025-02-11 21:49:22 +0000

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-# Ensure the script has execution permissions
-chmod +x build_files.sh
+#!/bin/bash
 
-# Install PostgreSQL dependencies
-apt-get update && apt-get install -y libpq-dev gcc
+# Install required system libraries
+apt-get update && apt-get install -y libffi-dev libcairo2-dev libpq-dev gcc
 
-# Create and activate a virtual environment
-python3 -m venv venv
-source venv/bin/activate
-
-# Ensure pip is installed and updated inside the virtual environment
+# Ensure pip is installed and upgraded
 python3 -m ensurepip --default-pip
 python3 -m pip install --upgrade pip
 
-# Install dependencies from requirements.txt inside the virtual environment
+# Create a virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Install dependencies
 pip install -r requirements.txt
 
 python3 -m pip install Django==5.1
