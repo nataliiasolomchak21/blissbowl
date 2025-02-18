0ed936c844ef27b9e9733c5b447d73e527c3c091
nataliiasolomchak21 <natalia2001727@gmail.com>
2025-02-18 19:31:58 +0000

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Install required system libraries
-apt-get update && apt-get install -y libffi-dev libcairo2-dev libpq-dev gcc
-
 # Ensure pip is installed and upgraded
 python3 -m ensurepip --default-pip
 python3 -m pip install --upgrade pip
