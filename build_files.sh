f1fff7c500dab1e4c932631c9432f5828bd6ef57
nataliiasolomchak21 <natalia2001727@gmail.com>
2025-02-18 19:34:42 +0000

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Ensure pip is installed and upgraded
-python3 -m ensurepip --default-pip
-python3 -m pip install --upgrade pip
-
-# Create a virtual environment
-python3 -m venv venv
-source venv/bin/activate
-
 # Install dependencies
 pip install -r requirements.txt
 
